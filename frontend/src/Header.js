a12b212032cfaa0f95a17a45e2287b95b1495595
PECHCHANAN SUWANRAK <6710110298@psu.ac.th>
2025-02-10 22:25:50 +0700

    editbug Mysql

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -2,17 +2,22 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div>
-            <nav className="flex space-x-4">
-                <a href="#" className="hover:text-gray-300">หน้าหลัก</a>
-                <a href="#" className="hover:text-gray-300">รายการทั้งหมด</a>
-                <a href="#" className="hover:text-gray-300">หมวดหมู่</a>
+        <header className="bg-gray-800 text-white fixed top-0 left-0 w-full shadow-md z-50">
+            <nav className="container mx-auto flex justify-between items-center py-4 px-6">
+                
+                <ul className="flex space-x-6">
+                    <li><a href="#" className="hover:text-gray-300">หน้าหลัก</a></li>
+                    <li><a href="#" className="hover:text-gray-300">รายการทั้งหมด</a></li>
+                    <li><a href="#" className="hover:text-gray-300">หมวดหมู่</a></li>
+                </ul>
+
+                
+                <ul className="flex space-x-6">
+                    <li><a href="#" className="hover:text-gray-300">เกี่ยวกับเรา</a></li>
+                    <li><a href="#" className="hover:text-gray-300">ติดต่อเรา</a></li>
+                </ul>
             </nav>
-            <div className="flex space-x-4">
-                <a href="#" className="hover:text-gray-300">เกี่ยวกับเรา</a>
-                <a href="#" className="hover:text-gray-300">ติดต่อเรา</a>
-            </div>
-        </div>
+        </header>
     );
 };
 
