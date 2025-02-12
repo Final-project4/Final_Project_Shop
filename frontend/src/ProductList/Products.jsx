f2ca8c202dd0e12a19ed85b95686522328026124
Krittikon <krittikon@example.com>
2025-02-13 00:27:58 +0700

    ProductsList

--- frontend/src/ProductList/Products.jsx
+++ frontend/src/ProductList/Products.jsx
@@ -7,9 +7,11 @@ const Products = () => {
       asdsadasdad
       <div>
         {/* Header section */}
-        <div>
+        <div className="text-center mb-10 max-w-[600px]">
             <p>Top Selling Products for you</p>
             <h1>Products</h1>
+            <p>Lorem ipsum dolor sit amet.
+            </p>
         </div>
         {/* Body section */}
       </div>
