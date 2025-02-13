01d24304a084e53b773e1da9be7c1248cec81419
PECHCHANAN SUWANRAK <6710110298@psu.ac.th>
2025-02-13 21:53:01 +0700

    add admin page

--- frontend/src/Route.js
+++ frontend/src/Route.js
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./HomeComponent/HomePage.js";
 import Products from "./ProductList/Products.jsx";
-
+import AdminPage from "./AdminPages/AdminPageAddItem.js";
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/products" element={<Products/>}/>
-      {/* <Route path="*" element={<NotFound />} /> */}
+      <Route path="adminpage" element={<AdminPage />} />
     </Routes>
   );
 };
