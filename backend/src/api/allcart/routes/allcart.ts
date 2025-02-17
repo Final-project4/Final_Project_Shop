cfd0aed83d10ceab5d4c1fac03b064283572e076
PECHCHANAN SUWANRAK <6710110298@psu.ac.th>
2025-02-17 07:27:57 +0700

    rebuild backend

--- backend/src/api/allcart/routes/allcart.ts
+++
@@ -1,7 +0,0 @@
-/**
- * allcart router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::allcart.allcart');
--- backend/src/api/allcart/services/allcart.ts
+++
@@ -1,7 +0,0 @@
-/**
- * allcart service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::allcart.allcart');
