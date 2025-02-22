4d5e79ff18c865495a3464cff4c5294b9cbe6b10
PECHCHANAN SUWANRAK <6710110298@psu.ac.th>
2025-02-22 19:42:41 +0700

    update before megre

--- frontend/src/Route.js
+++ frontend/src/Route.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import HomePage from "./HomeComponent/HomePage.js";
-import Products from "./ProductList/Products.jsx";
-import AdminPage from "./AdminPages/AdminPageAddItem.js";
-import AddCoupons from "./AdminPages/ADD_COUPONS.js";
-import OrderList from "./AdminPages/Order_List.js";
-import CartPage from "./CartComponent/Cart.js";
-import LoginScreen from "./LoginComponent/LoginPage.js";
-import ItemDetail from "./ItemDetail/ItemDetail.js";
+  import React from "react";
+  import { Routes, Route } from "react-router-dom";
+  import HomePage from "./HomeComponent/HomePage.js";
+  import Products from "./ProductList/Products.jsx";
+  import AdminPage from "./AdminPages/AdminPageAddItem.js";
+  import AddCoupons from "./AdminPages/ADD_COUPONS.js";
+  import OrderList from "./AdminPages/Order_List.js";
+  import CartPage from "./CartComponent/Cart.js";
+  import LoginScreen from "./LoginComponent/LoginPage.js";
+  import ItemDetail from "./ItemDetail/ItemDetail.js";
 
-const AppRoutes = () => (
+  const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/products" element={<Products />} />
