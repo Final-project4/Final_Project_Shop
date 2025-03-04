5168157ada165838a913d8dca24841c2d91c4252
Tanawat Udomcharoensab <6710110579@psu.ac.th>
2025-03-04 15:03:17 +0700

    add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,21 @@ import Header from "./Header";
 import AppRoutes from "./Route";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./ItemDetail/CartContext";
+import Footer from "./FooterComponent/Footer";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <Header />
-        <CartProvider>
-          <AppRoutes />
-        </CartProvider>
-        
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow">
+            <CartProvider>
+              <AppRoutes />
+            </CartProvider>
+          </main>
+          <Footer />
+        </div>
       </Router>
     </AuthProvider>
   );
